a32984b892bcc4b2dc6c8c22532d3cfba858aac7
Garg Ayush <ayush.garg@ab-inbev.com>
2022-01-04 21:13:27 +0530

    AB#8350 DataScience: Modularize SC

---
+++ utils/DataIngestion.py
@@ -0,0 +1,28 @@
+from queries import Queries as Q
+import numpy as np
+import pandas as pd
+
+class DataIngestion:
+
+    def __init__():
+        super().__init__()
+
+
+    @classmethod
+    def get_baseline_df(cls, pillar, month):
+        
+        
+    @classmethod
+    def get_treatment_df(cls, pillar, month):
+
+
+    @classmethod
+    def get_baseline_sku_df(cls, pillar, month):
+        
+        
+    @classmethod
+    def get_treatment_sku_df(cls, pillar, month):
+       
+        
+    @classmethod
+    def get_treatment_df_2(cls, pillar, month):
